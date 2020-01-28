fd3ba727c183487668d119b2081c022db90d78a2
BranceA <brancealexander@gmail.com>
2020-01-28 13:26:23 -0600

    solution to problem 1

---
+++ src/ProblemOne.java
@@ -0,0 +1,19 @@
+public class ProblemOne {
+
+    public static void main(String[] args) {
+        System.out.println(multipleOfThreeAndFive());
+    }
+
+    public static int multipleOfThreeAndFive(){
+        int total = 0;
+        for (int i = 1; i < 1000; i++){
+            if(i % 3 == 0 || i % 5 == 0){
+                System.out.println("i = " + i);
+                total += i;
+                System.out.println("total = " + total);
+            }
+        }
+        return total;
+    }
+
+}
