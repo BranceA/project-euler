a684f0bbe49e940228494e83610455b83b1579c3
BranceA <brancealexander@gmail.com>
2020-01-29 16:41:55 -0600

    brute force solution to problem 5

--- src/Problem5.java
+++ src/Problem5.java
@@ -4,10 +4,14 @@
 
 public class Problem5 {
     public static void main(String[] args) {
-
+        System.out.println(smallestMultiple());
     }
 
     private static int smallestMultiple(){
-
+        for(int i = 20; true; i++){
+            if(i % 20 == 0 && i % 19 == 0 && i % 18 == 0 && i % 17 == 0 && i % 16 == 0 && i % 15 == 0 && i % 13 == 0 && i % 11 == 0 && i % 9 == 0 && i % 8 == 0 && i % 7 == 0){
+                return i;
+            }
+        }
     }
 }
