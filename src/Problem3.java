6ff9ddc6950a6134a155f332397a4e6290cdb443
BranceA <brancealexander@gmail.com>
2020-01-29 12:11:57 -0600

    possibly takes less than an hour to run

--- src/Problem3.java
+++ src/Problem3.java
@@ -4,14 +4,23 @@ public class Problem3 {
     }
 
     private static long largestPrimeFactor(){
-        for(long i = 600851475143L; i > 1L; i--){
+        long currentHighest = 2;
+        for(long i = 2L; i < 300425737572L; i++){
             if(600851475143L % i == 0) {
+                long otherFactor = 600851475143L/2;
                 if (isPrime(i)) {
-                    return i;
+                    if(i > currentHighest){
+                        currentHighest = i;
+                    }
+                }
+                if(isPrime(otherFactor)){
+                    if(otherFactor > currentHighest){
+                        currentHighest = otherFactor;
+                    }
                 }
             }
         }
-        return 1L;
+        return currentHighest;
     }
 
     private static boolean isPrime(long numToCheck){
@@ -23,3 +32,4 @@ public class Problem3 {
         return true;
     }
 }
+//300425737572L half of big number
